4521cd97a808aa738294e5d99df5c497609c055b
lunarcao <datcungkho0@gmail.com>
2024-10-30 13:55:20 +0700

    First commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=cnd
+export POSTGRES_PASSWORD=thangthankinh98
+export POSTGRES_HOST=cdr.c4fdkdtbnez8.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=test-3421927-dev
+export AWS_REGION=us-east-1a
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
